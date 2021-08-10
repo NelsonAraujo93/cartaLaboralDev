76a4954a5ae68afac9a2ffdffc845996b47d903b
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2021-08-10 08:48:28 -0500

    ss

--- controllers/dbConecction.js
+++ controllers/dbConecction.js
@@ -9,7 +9,8 @@ const connection = mysql.createConnection({
   host     : process.env.DATABASE_HOST,
   user     : process.env.DATABASE_USER,
   password : process.env.DATABASE_PASSWORD,
-  //port : 3306,
-  database : process.env.DATABASE
+  port : 3306,
+  database : process.env.DATABASE,
+  insecureAuth : true
 });
 module.exports = connection;
