f2a10b0588ea38da8f1614951703be4a09c9046a
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2021-08-17 10:52:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 //const cookieparser= require ('cookie-parser');
-
+const path = require('path');
 const app = express();
 
 //ejecutar express
