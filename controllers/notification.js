79a37dfbd52ebb0f80a1bb71c32a4f736d582496
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2021-08-17 10:17:41 -0500

    mailer

--- controllers/notification.js
+++ controllers/notification.js
@@ -6,7 +6,7 @@ const dbConnection = require('./dbConecction');
 var bcrypt = require('bcryptjs');
 const { promisify } = require ('util');
 const path = require ('path');
-
+const mailer = require('../controllers/mailer');
 const jwt = require ('jsonwebtoken');
 //const configMensaje = require('../controllers/configMensajes');
 
@@ -263,7 +263,7 @@ var controller = {
                 if(!result){
                     return res.status(404).send({
                         status: 'error',
-                        message: 'El usuario no esta registrado'
+                        message: err
                     });
                 } else {
                     if(await bcrypt.compareSync(params.pass, result[0].pass)){
@@ -359,6 +359,7 @@ var controller = {
                 state: params.state,
                 checked: params.checked,
                 admin_id: params.admin_id,
+                observations: params.observations
             };
             var update=[
                 form,
@@ -455,7 +456,7 @@ var controller = {
      */
     getFormById:  (req, res ) => {
         var params= req.params;
-        dbConnection.query('SELECT forms.request_type,forms.admin_id, forms.state, forms.checked, forms.id, forms.content,users.id as user_id,users.name,users.identification,users.telephone,users.email,stamps.id as stamp_id, stamps.stamp_url FROM `forms` INNER JOIN `users` ON forms.user_id=users.id INNER JOIN `stamps` ON forms.stamp_id=stamps.id where forms.id = ?', params.id ,(err, result) => {
+        dbConnection.query('SELECT forms.observations,forms.request_type,forms.admin_id, forms.state, forms.checked, forms.id, forms.content,users.id as user_id,users.name,users.identification,users.telephone,users.email,stamps.id as stamp_id, stamps.stamp_url FROM `forms` INNER JOIN `users` ON forms.user_id=users.id INNER JOIN `stamps` ON forms.stamp_id=stamps.id where forms.id = ?', params.id ,(err, result) => {
             if (err){
                 return res.status(404).send({
                     status: 'error',
@@ -798,6 +799,14 @@ var controller = {
                 mesage: 'Formulario mal digilenciado'
             });
         };
+    },
+    formularioCorreo: (req, res) => {
+        mailer(req.body);
+        console.log(req);
+        return res.status(200).send({
+            status: 'Ok',
+            mesage: 'Mensaje enviado'
+        });
     },
 };
 module.exports = controller;
