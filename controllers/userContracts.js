005b9c16e978a0acd4c54cba4690468f572efc9d
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2021-08-17 14:57:32 -0500

    Update userContracts.js

--- controllers/userContracts.js
+++ controllers/userContracts.js
@@ -241,6 +241,7 @@ var controllerDB2 = {
      */
      createContractor:  (req, res ) => {
         var params = req.body;
+        console.log(params)
         try {
             var validate_identification = !validator.isEmpty(toString(params.identification));
             var validate_name = !validator.isEmpty(params.name);
